1550692b933a6c1d93fc3e1a6177243be596fc9b
Shubo Yang <yangshubo@outlook.com>
2015-11-06 12:11:48 +0800

    fixed compiling errors

--- include/maraton-server.h
+++ include/maraton-server.h
@@ -9,20 +9,12 @@
 #define MARATON_SERVER_H_ 
 
 #include "maraton.h"
-#include <stdio.h>
-#include <locale>
-#include <codecvt>
+#include <stdio.h> 
 
 #define PP_DEF(__type__,__name__) void __name__( __type__ value ) { __name__##_ = value; }; \
               __type__ __name__() { return __name__##_; }
 
 #define EXECUTOR_TIMEOUT 5000000
-
-inline std::wstring utf8_2_ws( const std::string& src )
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t> > conv;
-    return conv.from_bytes( src );
-}
-
+ 
 #endif //!MARATON_SERVER_H_ 
 
